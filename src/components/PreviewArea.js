d145fcb7d65553bf9bc265b2bb239665f113401b
snjwilson <sanjaywilsonktm@gmail.com>
2021-07-25 16:48:43 +0530

    border bottom rounded and added margin between preview area and all sprites area

--- src/components/PreviewArea.js
+++ src/components/PreviewArea.js
@@ -3,7 +3,7 @@ import CatSprite from "./CatSprite";
 
 export default function PreviewArea() {
   return (
-    <div className="flex-none h-full overflow-y-auto p-2">
+    <div className="flex-none h-1/2 bg-white overflow-y-auto p-2 border-b rounded-bl-xl">
       <CatSprite />
     </div>
   );
