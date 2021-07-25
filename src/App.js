1a590f0fb9f64ffc9bef0e5bd6e7d870f336b0ba
snjwilson <sanjaywilsonktm@gmail.com>
2021-07-25 16:47:29 +0530

    state for setting the current category chosen

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "./components/Sidebar";
 import MidArea from "./components/MidArea";
 import PreviewArea from "./components/PreviewArea";
+import SpritesArea from "./components/SpritesArea";
 
 export default function App() {
+  const [category, setCategory] = useState("motion");
   return (
     <div className="bg-blue-100 pt-6 font-sans">
       <div className="h-screen overflow-hidden flex flex-row  ">
-        <div className="flex-1 h-screen overflow-hidden flex flex-row bg-white border-t border-r border-gray-200 rounded-tr-xl mr-2">
-          <Sidebar /> <MidArea />
+        <div className="flex-1 h-full overflow-hidden flex flex-row bg-white border-t border-r border-gray-200 rounded-tr-xl mr-2">
+          <Sidebar category={category} setCategory={setCategory} /> <MidArea />
         </div>
-        <div className="w-1/3 h-screen overflow-hidden flex flex-row bg-white border-t border-l border-gray-200 rounded-tl-xl ml-2">
+        <div className="w-1/3 h-full overflow-hidden flex flex-col border-t border-l border-gray-200 rounded-tl-xl ml-2">
           <PreviewArea />
+          <SpritesArea />
         </div>
       </div>
     </div>
